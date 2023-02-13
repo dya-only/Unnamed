9000a3e6ddff029f657ba456c9f8f8a0f2615ae7
dya-code <dyacode@proton.me>
2023-02-14 00:14:11 +0900

    search pv video

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,9 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import laftel from 'laftel.js'
+import SearchYT from 'youtube-search'
+
+const opts: SearchYT.YouTubeSearchOptions = {
+  maxResults: 1,
+  key: "AIzaSyBGJ0_EWoFbTmXu6q19jmqBDGOQ46hqxcY"
+}
 
 type Data = {
-  anime: any
+  anime: any,
+  pv: string
 }
 
 export default async function handler(
@@ -12,15 +19,23 @@ export default async function handler(
 ) {
   if (req.method === 'GET') {
     const params = req.query as any
+    let pv = ''
 
     await laftel.search(params.name).then(result => {
       const [anime] = result.results
       if (anime != null || anime != undefined) {
         laftel.getItem(anime.id).then(result => {
-          res.status(200).json({ anime: result })
+
+          SearchYT(`${result.name} PV`, opts, (err, results) => {
+            if(err) return console.log(err)
+          
+            pv = results[0].link
+            res.status(200).json({ anime: result, pv: pv })
+          })
+
         })
       } else {
-        res.status(200).json({ anime: 'not found' })
+        res.status(200).json({ anime: 'not found', pv: '' })
       }
     })
   } else {
