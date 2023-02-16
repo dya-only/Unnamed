0a594b5595df35a609ce57d162d47e437ef44e05
dya-code <dyacode@proton.me>
2023-02-16 16:41:46 +0900

    kakao api fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,13 @@ import '@/styles/globals.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import type { AppProps } from 'next/app'
 
+declare global {
+  interface Window {
+    Kakao: any;
+    naver: any;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return <GoogleOAuthProvider clientId='870072489503-28hbupk1iscump8po398uqgpp8g4kl5r.apps.googleusercontent.com'>
     <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
